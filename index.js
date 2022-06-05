eded80297d09274c078c50c673a301202f27a2fe
JOY <joy.kemunto@student.moringaschool.com>
2022-06-05 12:22:45 +0300

    commit tree 256731630fa88bc633cdbb17cf51aff597b6e6cb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// Name my employee to be a receptionist //
+const employee = {
+    name: "Joy",
+    streetAddress: "Uthiru",
+  };
+  
+  function updateEmployeeWithKeyAndValue(receptionist, key, value) {
+    return Object.assign({}, receptionist, { [key]: value });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(receptionist, key, value) {
+    receptionist[key] = value;
+  
+    return receptionist;
+  }
+  
+  function deleteFromEmployeeByKey(receptionist, key) {
+    const newObj = Object.assign({}, receptionist);
+    delete newObj[key];
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(receptionist, key) {
+    delete receptionist[key];
+  
+    return receptionist;
+  }
